862aa706dd1b34c0ae5dd412be85b6c417dd30bd
Yigit <recepyigitk@gmail.com>
2018-02-15 21:22:34 +0300

    Version 1

---
+++ Calculator/Operations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Calculator
+{
+    public class Operations
+    {
+        public Operations()
+        {
+
+        }
+        public int Topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Cikar(int sayi1, int sayi2)
+        {
+            return sayi1-sayi2;
+        }
+        public double Bol(float sayi1, float sayi2)
+        {
+            return sayi1 / sayi2;
+        }
+
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+
+       
+
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,80 @@
+﻿//Yiğit Recep Kader
+//15253027
+
+using System;
+
+namespace Calculator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool flag=true;
+            int sayi1=0;
+            int sayi2=0;
+            Operations op = new Operations();
+
+            while (flag==true)
+            {
+                Console.WriteLine("Islemler : Toplama(+) - Cikarma(-) - Carpma(*) - Bolme(/)");
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Islemi seciniz ");
+                string deger = Console.ReadLine();
+                Console.WriteLine("1. sayiyi giriniz");
+                sayi1 = Convert.ToInt32(Console.ReadLine());
+
+               
+                    Console.WriteLine("2. sayiyi giriniz");
+                    sayi2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine();
+
+
+                   
+                    if (deger == "+")
+                    {
+                        Console.WriteLine("Sonuc: " + op.Topla(sayi1, sayi2));
+                    }
+
+
+                    else if (deger == "-")
+                    {
+                        Console.WriteLine("Sonuc: " + op.Cikar(sayi1, sayi2));
+                    }
+
+                    else if (deger == "*")
+                    {
+                        Console.WriteLine("Sonuc :" + op.Carp(sayi1, sayi2));
+                    }
+
+
+                    else if (deger == "/")
+                    {
+                        Console.WriteLine("Sonuc:" + op.Bol(sayi1, sayi2));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tanımsız işlem");
+                    }
+                    Console.WriteLine("Devam etmek (e) ye basınız");
+                    string deger2 = Console.ReadLine();
+
+                    if (deger2 == "e")
+                    {
+                        flag = true;
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+
+
+
+             
+            }
+          
+        }
+    }
+}
